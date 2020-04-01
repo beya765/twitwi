5275f6097ae1fa1871f80b1f2bef89565c086649
beya765 <kenshinpower2000@yahoo.co.jp>
2020-04-01 02:06:15 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザ登録ありがとうございます！"
+      redirect_to @user # redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    # ユーザ登録で不正なデータが渡ってこないように内容指定
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                          :password_confirmation)
+    end
 end
